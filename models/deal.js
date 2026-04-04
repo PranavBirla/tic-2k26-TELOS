8c9dcf7c244e01c7b0f2b3a0cfcd711f4c2d294c
Pranav Birla <pranavbirla06@gmail.com>
2026-04-04 09:15:48 +0530

    Added deal and negotiation system and deal managements for both sides

---
+++ models/deal.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const dealSchema = new mongoose.Schema({
+    cropId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "crop",
+        required: true
+    },
+    farmerId:{
+        // type: mongoose.Schema.Types.ObjectId,
+        // ref: "user",
+        type: String,
+        default: "demoFarmer"
+
+    },
+    buyerId: {
+        // type: mongoose.Schema.Types.ObjectId,
+        // ref: "user"
+        type: String,
+        default: "demoBuyer"
+    },
+    buyerContact: Number,
+    farmerContact: Number,
+    quantity: Number,
+    offeredPrice: Number,
+    farmerName: String,
+    dealTime: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: String,
+        enum: ["pending", "accepted", "rejected"],
+        default: "pending"
+    }
+});
+
+const deal = mongoose.model("deal", dealSchema);
+
+module.exports = deal;
